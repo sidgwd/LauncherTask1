4c2434960b0b7151f9e749458946020c85477b94
Siddhesh <siddhesh.gawde@vxlsoftware.com>
2020-12-06 14:20:49 +0530

    first commit

---
+++ SDK/src/main/java/com/sid/sdk/INotifyAppList.java
@@ -0,0 +1,5 @@
+package com.sid.sdk;
+
+public interface INotifyAppList {
+    void appListUpdated();
+}
---
+++ app/src/main/java/com/sid/jiolauncher/Activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.sid.jiolauncher.Activity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sid.jiolauncher.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                startActivity(new Intent(SplashActivity.this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                finish();
+            }
+        }, 3000);
+        getPackageManager().setComponentEnabledSetting(new ComponentName(getPackageName(),
+                MainActivity.class.getName()), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+
+    }
+
+
+}
